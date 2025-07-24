8e6b073706f72c103ccd6c29d045f3c5d31f15a6
jewelcodes <omarmelghoul01@gmail.com>
2025-07-24 00:10:03 -0400

    frontend: fix for overlays and backgrounds

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     });
 
     return (
-        <main>
+        <main className="transparent">
             <Header />
             <Search handler={setState} />
             <EtymologyTree
